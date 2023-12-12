62a731e5c203ae4b87096b568d586fe6b4346b03
HarryR-P <har1@iastate.edu>
2023-12-11 22:51:24 -0600

    up

--- eval.py
+++ eval.py
@@ -23,9 +23,9 @@ warnings.simplefilter("ignore")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
-    env = gym.make('ALE/Breakout-v5', obs_type="ram", frameskip=4)
+    env = gym.make('ALE/Breakout-v5', obs_type="ram", render_mode='rgb_array', frameskip=4)
     env = FrameStack(env, 4)
-    # env = RecordVideo(env, video_folder='data\\final_video', episode_trigger=lambda t: True, disable_logger=True)
+    env = RecordVideo(env, video_folder='data\\final_video', episode_trigger=lambda t: True, disable_logger=True)
     n_actions = env.action_space.n
     state, info = env.reset()
     state_shape = state.shape[0] * state.shape[1]
@@ -33,7 +33,8 @@ def main():
     policy_net.load_state_dict(torch.load('data\\model_best.pth'))
     state = torch.tensor(state, dtype=torch.float32, device=device).view(-1).unsqueeze(0)
     total_reward = 0.0
-    for ep in range(100):
+    max_reward = 33.0
+    for ep in range(500):
         running_reward = 0.0
         done = False
         state, info = env.reset()
@@ -50,16 +51,16 @@ def main():
                 else:
                     next_state = torch.tensor(observation, dtype=torch.float32, device=device).view(-1).unsqueeze(0)
                 state = next_state
-                # env.render()
-        # env.close_video_recorder()
+                env.render()
+        env.close_video_recorder()
         print(f'Reward: {running_reward}')
         # time.sleep(1)
-        # if running_reward > max_reward:
-        #     max_reward = running_reward
-        #     if not os.path.exists(f'data\\saved_final_videos\\final_video_{int(running_reward)}r'):
-        #         os.mkdir(f'data\\saved_final_videos\\final_video_{int(running_reward)}r')
-        #     shutil.copy(f'data\\final_video\\rl-video-episode-{ep}.meta.json', f'data\\saved_final_videos\\final_video_{int(running_reward)}r\\rl-video-episode-{ep}.meta.json')
-        #     shutil.copy(f'data\\final_video\\rl-video-episode-{ep}.mp4', f'data\\saved_final_videos\\final_video_{int(running_reward)}r\\rl-video-episode-{ep}.mp4')
+        if running_reward > max_reward:
+            max_reward = running_reward
+            if not os.path.exists(f'data\\saved_final_videos\\final_video_{int(running_reward)}r'):
+                os.mkdir(f'data\\saved_final_videos\\final_video_{int(running_reward)}r')
+            shutil.copy(f'data\\final_video\\rl-video-episode-{ep}.meta.json', f'data\\saved_final_videos\\final_video_{int(running_reward)}r\\rl-video-episode-{ep}.meta.json')
+            shutil.copy(f'data\\final_video\\rl-video-episode-{ep}.mp4', f'data\\saved_final_videos\\final_video_{int(running_reward)}r\\rl-video-episode-{ep}.mp4')
     print(f'Average Reward: {total_reward/100}')
     return
 
